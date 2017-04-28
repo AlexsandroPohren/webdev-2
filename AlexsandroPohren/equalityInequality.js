aa8d593ee4ed9cfdca3baca1b41a8215d6957921
Alexsandro Pohren <alexsandroantonio65@outlook.com>
2017-04-27 21:08:33 -0300

    Equality & Inequality

---
+++ AlexsandroPohren/equalityInequality.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var a =  42;
+var b =  "72";
+
+//Equality
+a == b; //coercion
+a === b;
+
+// Not Equality
+a != b;
+a !== b;
+
+//Inequality - não é not Equality !!!
+
+var ehMaior = a > b; // boolean
+var ehMenor = a < b; // boolean
+
+var str1 = "casa";
+var str2 = "abacaxi";
+
+str1 > str2; // true or false ???
+// usado para numeros
+// usado para String
+// coercion/ coerção
+
+// condicionais é uma divisão de codigos, onde vai executar determinado bloco de codigo sobre uma determinada condição
+// exemplo de condicionais: if - else, for, while.
